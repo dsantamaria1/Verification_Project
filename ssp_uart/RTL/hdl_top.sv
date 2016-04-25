7df07befa17ce523cf9f0e7eb958e8292818afb9
Daniel Santa Maria <santamaria@utexas.edu>
2016-04-25 17:39:45 -0500

    parametrized memory

--- ssp_uart/RTL/hdl_top.sv
+++ ssp_uart/RTL/hdl_top.sv
@@ -62,28 +62,31 @@ module hdl_top;
    // -- DUT (Design Under Test)
    // ----------------------------------------------------------------
 
-   SSP_UART ssp_uart(
-    .Rst(Rst),			// System Reset
-    .Clk(Clk),			// System Clock
-    .SSP_SSEL(SSP_SSEL),	// SSP Slave Select
-    .SSP_SCK(SSP_SCK),		// Synchronous Serial Port Serial Clock
-    .SSP_RA(SSP_RA),		// SSP Register Address
-    .SSP_WnR(SSP_WnR),		// SSP Command
-    .SSP_En(SSP_En),		// SSP Start Data Transfer Phase (Bits 11:0)
-    .SSP_EOC(SSP_EOC),		// SSP End-Of-Cycle (Bit 0)
-    .SSP_DI(SSP_DI),		// SSP Data In
-    .RxD_232(RxD_232),		// RS-232 Mode RxD
-    .xCTS(xCTS),		// RS-232 Mode CTS (Okay-To-Send)
-    .RxD_485(RxD_485),		// RS-485 Mode RxD
-    .SSP_DO(SSP_DO),		// SSP Data Out
-    .TxD_232(TxD_232),		// RS-232 Mode TxD
-    .xRTS(xRTS),		// RS-232 Mode RTS (Ready-To-Receive)
-    .TxD_485(TxD_485),		// RS-485 Mode TxD
-    .xDE(xDE),			// RS-485 Mode Transceiver Drive Enable
-    .IRQ(IRQ),			// Interrupt Request
-    .TxIdle(TxIdle),
-    .RxIdle(RxIdle)
-);
+   SSP_UART #(
+             	.pTF_Depth(3),
+		.pRF_Depth(3)
+	     ) ssp_uart(
+    		.Rst(Rst),			// System Reset
+    		.Clk(Clk),			// System Clock
+    		.SSP_SSEL(SSP_SSEL),	// SSP Slave Select
+    		.SSP_SCK(SSP_SCK),		// Synchronous Serial Port Serial Clock
+    		.SSP_RA(SSP_RA),		// SSP Register Address
+    		.SSP_WnR(SSP_WnR),		// SSP Command
+    		.SSP_En(SSP_En),		// SSP Start Data Transfer Phase (Bits 11:0)
+    		.SSP_EOC(SSP_EOC),		// SSP End-Of-Cycle (Bit 0)
+    		.SSP_DI(SSP_DI),		// SSP Data In
+    		.RxD_232(RxD_232),		// RS-232 Mode RxD
+    		.xCTS(xCTS),		// RS-232 Mode CTS (Okay-To-Send)
+    		.RxD_485(RxD_485),		// RS-485 Mode RxD
+    		.SSP_DO(SSP_DO),		// SSP Data Out
+    		.TxD_232(TxD_232),		// RS-232 Mode TxD
+    		.xRTS(xRTS),		// RS-232 Mode RTS (Ready-To-Receive)
+    		.TxD_485(TxD_485),		// RS-485 Mode TxD
+    		.xDE(xDE),			// RS-485 Mode Transceiver Drive Enable
+    		.IRQ(IRQ),			// Interrupt Request
+    		.TxIdle(TxIdle),
+    		.RxIdle(RxIdle)
+  	     );
 
    function void get_interface(output virtual ssp_uart_if ssp_uart_vif);
        ssp_uart_vif = ssp_uart_cfg_if_i;
