6cec6d2b1a01c56a84db6d9464f28c9a26fa0178
Daniel Santa Maria <santamaria@utexas.edu>
2016-04-28 17:49:13 -0500

    created TFIFO and RFIFO base classes

--- ssp_uart/Sim/tests.sv
+++ ssp_uart/Sim/tests.sv
@@ -38,6 +38,7 @@ class test_base;
     ssp_uart_vif.SSP_En_sig 	= item.get_SSP_En();
     ssp_uart_vif.RxD_232_sig	= item.get_RxD_232();
     $display("driving transaction at time: %0t", $time);
+    item.print();
   endtask
 
   function checkExpectedValue(logic [11:0] expected, logic [11:0] actual);
@@ -50,6 +51,213 @@ class test_base;
   endfunction 
 
 endclass: test_base
+
+
+class TFIFO_base extends test_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+  task enableInterrupt;
+      //enable interrupt
+      c1 = new(ssp_uart_vif);
+      ssp_di = 'h100;
+      c1.set_SSP_RA(`UCR);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1); 
+
+      repeat(10) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+  endtask: enableInterrupt
+
+
+  task clearTFifoInterrupts;
+      c1 = new(ssp_uart_vif);
+      ssp_di = 'h02;
+      ssp_en = 1'b1;
+      c1.set_SSP_RA(`USR);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1);
+
+      // wait for USR[0] to toggle to clear iTFE/iTHE
+      @(posedge ssp_uart_vif.usr_0);
+
+      c1.set_SSP_En(ssp_en);
+      drive_transaction(c1); 
+       
+      repeat(256) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+  endtask: clearTFifoInterrupts
+
+
+  task populateTFIFO(int numTransactions);
+      //Write data to TDR to go into Transmit FIFO
+      c1 = new(ssp_uart_vif);
+      ssp_di = 'h0E1;
+      c1.set_SSP_RA(`TDR);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1);
+      repeat(6) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+      
+      // Call task drive_transaction with config_item c1 as argument
+      for(i=0; i < numTransactions; i++) begin
+        ssp_di++;
+        c1.set_SSP_DI(ssp_di);
+        drive_transaction(c1);
+        repeat(6) begin
+          @(negedge ssp_uart_vif.Clk_sig);
+        end
+      end
+
+  endtask: populateTFIFO
+  
+
+  task clearTFIFO();
+      // clear fifo 
+      ssp_di = 'h800; //TFC is bit 11
+      c1 = new(ssp_uart_vif);
+      c1.set_SSP_RA(`TDR);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      
+      drive_transaction(c1);
+      repeat(6) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+
+  endtask: clearTFIFO
+
+endclass: TFIFO_base
+
+
+
+
+class RFIFO_base extends test_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+  task clearFifoInterrupts;
+      c1 = new(ssp_uart_vif);
+      ssp_di = 'h02;
+      ssp_en = 1'b1;
+      c1.set_SSP_RA(`USR);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1);
+
+      // wait for USR[0] to toggle to clear iTFE/iTHE
+      @(posedge ssp_uart_vif.usr_0);
+
+      c1.set_SSP_En(ssp_en);
+      drive_transaction(c1); 
+       
+      repeat(256) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+  endtask: clearFifoInterrupts
+
+
+  task enableInterrupt;
+      //enable interrupt
+      c1 = new(ssp_uart_vif);
+      ssp_di = 'h100;
+      c1.set_SSP_RA(`UCR);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1); 
+
+      repeat(10) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+  endtask: enableInterrupt
+
+
+  task clearRFIFO();
+      ssp_di = 'h400; //RFC is bit 10
+      c1 = new(ssp_uart_vif);
+      c1.set_SSP_RA(`TDR);
+      c1.set_SSP_WnR(`WRITE);
+      c1.set_SSP_DI(ssp_di);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
+      drive_transaction(c1);
+      repeat(10) begin
+        @(negedge ssp_uart_vif.Clk_sig);
+      end
+  endtask: clearRFIFO
+
+
+  task populateRFIFO(int numTransactions);
+      int j =0;     
+      for(i=0; i<numTransactions; i++) begin
+        rxd_232 = 1;
+        c1 = new(ssp_uart_vif);
+        c1.set_RxD_232(rxd_232);
+        drive_transaction(c1);
+
+        repeat(5) begin
+          @(negedge ssp_uart_vif.SSP_SCK_sig);
+        end
+
+        //send start bit
+        rxd_232 = 0;
+        $display("rxd_232 = %0h", rxd_232);
+        c1.set_RxD_232(rxd_232);
+        drive_transaction(c1);
+        
+        repeat(10) begin
+          @(negedge ssp_uart_vif.SSP_SCK_sig);
+        end
+        
+        for(j=0; j<8; j++) begin
+          rxd_232 = $urandom();
+          $display("rxd_232 = %0h", rxd_232);
+          c1.set_RxD_232(rxd_232);
+          drive_transaction(c1);
+          repeat(64) begin
+            @(negedge ssp_uart_vif.Clk_sig);
+          end
+        end 
+
+        //send stop bit
+        rxd_232 = 1;
+        $display("rxd_232 = %0h", rxd_232);
+        c1.set_RxD_232(rxd_232);
+        drive_transaction(c1);
+        
+        repeat(10) begin
+          @(negedge ssp_uart_vif.SSP_SCK_sig);
+        end
+
+     end// for
+
+  endtask: populateRFIFO
+  
+
+endclass: RFIFO_base
+
+
+
 
 
 class uart_reg_rw extends test_base;
@@ -73,8 +281,6 @@ class uart_reg_rw extends test_base;
       c1.set_SSP_SSEL(ssp_ssel);
       c1.set_SSP_EOC(ssp_eoc);
 
-      // Call method print for c1
-      c1.print();
       // Call task drive_transaction with config_item c1 as argument
       drive_transaction(c1);
       
@@ -137,69 +343,31 @@ endclass: uart_reg_init
 
 
 
-class tfifo_clear extends test_base;
+//////// Put data in Transmit FIFO //////////////
+class tfifo_clear extends TFIFO_base;
     function new(virtual ssp_uart_if ssp_uart_vif);
       super.new(ssp_uart_vif);
     endfunction: new
         
-//////// Put data in Transmit FIFO //////////////
     task run();
       ssp_eoc = 1'b1;
       ssp_ssel = 1'b1;
-      ssp_en = 1'b1;
 
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h04;
-      c1.set_SSP_RA(`USR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      c1.print();
-      drive_transaction(c1);
+      clearTFifoInterrupts();
 
-      // wait for USR[0] to toggle to clear iTFE/iTHE 
-      @(posedge ssp_uart_vif.usr_0);
-
-      c1.set_SSP_En(ssp_en);
-      drive_transaction(c1); 
-       
-      repeat(256) begin
-        @(negedge ssp_uart_vif.Clk_sig);
-      end
-
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h0F1;
-      c1.set_SSP_RA(`TDR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      
-      // Call task drive_transaction with config_item c1 as argument
-      for(i=0; i < 8; i++) begin
-        drive_transaction(c1);
-        ssp_di++;
-        c1.set_SSP_DI(ssp_di);
-        repeat(5) begin
-          @(negedge ssp_uart_vif.Clk_sig);
-        end
-      end
-      #150; 
-     
-      // clear fifo 
-      ssp_di = 'h800; //TFC is bit 11
-      c1.set_SSP_DI(ssp_di);
-      drive_transaction(c1);
-      repeat(2) begin
-        @(negedge ssp_uart_vif.SSP_SCK_sig);
-      end
+      populateTFIFO(6);
       
       //Stop writing to TDR
-      c1.set_SSP_WnR(`READ);
+      c1 = new(ssp_uart_vif);
       c1.set_SSP_RA('h7);
+      c1.set_SSP_WnR(`READ);
+      c1.set_SSP_SSEL(ssp_ssel);
+      c1.set_SSP_EOC(ssp_eoc);
       drive_transaction(c1);
-      #2000;
+     
+      
+      clearTFIFO();
+      #5us;
 
       //TODO: check that fifo was cleared 
     endtask: run
@@ -207,78 +375,10 @@ class tfifo_clear extends test_base;
 endclass:tfifo_clear
 
 
-class rfifo_clear extends test_base;
-    function new(virtual ssp_uart_if ssp_uart_vif);
-      super.new(ssp_uart_vif);
-    endfunction: new
-
-//////// Put data in Receive FIFO //////////////
-    task run();
-      ssp_uart_reset();
-      ssp_eoc = 1'b1;
-      ssp_ssel = 1'b1;
-
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h04;
-      c1.set_SSP_RA(`USR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      c1.print();
-      drive_transaction(c1);
-
-      //TODO: change to iTFE/iTHE going low (Maybe not for rfifo?)
-      repeat(256) begin
-        @(negedge ssp_uart_vif.Clk_sig);
-      end
-
-      for(i=0; i<2; i++) begin
-        c1 = new(ssp_uart_vif);
-        rxd_232 = 1;
-        c1.set_RxD_232(rxd_232);
-        drive_transaction(c1);
-
-        repeat(5) begin
-          @(negedge ssp_uart_vif.SSP_SCK_sig);
-        end
-        
-        rxd_232 = 0;
-        c1.set_RxD_232(rxd_232);
-        drive_transaction(c1);
-        #5000;
-        
-        rxd_232 = 1;
-        c1.set_RxD_232(rxd_232);
-        drive_transaction(c1);
-        #5000;
-        
-        rxd_232 = 0;
-        c1.set_RxD_232(rxd_232);
-        drive_transaction(c1);
-        #5000;
-        
-        rxd_232 = 1;
-        c1.set_RxD_232(rxd_232);
-        drive_transaction(c1);
-        #5000;
-      end 
- 
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h400;
-      c1.set_SSP_RA(`TDR);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      drive_transaction(c1);
-  
-      #500;
-    endtask: run
-endclass: rfifo_clear
 
 
-class the_interrupt extends test_base;
+
+class the_interrupt extends TFIFO_base;
     function new(virtual ssp_uart_if ssp_uart_vif);
       super.new(ssp_uart_vif);
     endfunction: new
@@ -286,61 +386,11 @@ class the_interrupt extends test_base;
     task run();
       ssp_eoc = 1'b1;
       ssp_ssel = 1'b1;
-      ssp_en = 1'b1;
 
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h04;
-      c1.set_SSP_RA(`USR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      c1.print();
-      drive_transaction(c1);
+      clearTFifoInterrupts();
+      enableInterrupt();
 
-      // wait for USR[0] to toggle to clear iTFE/iTHE
-      @(posedge ssp_uart_vif.usr_0);
-
-      c1.set_SSP_En(ssp_en);
-      drive_transaction(c1); 
-       
-      repeat(256) begin
-        @(negedge ssp_uart_vif.Clk_sig);
-      end
-
-      //enable interrupt
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h100;
-      c1.set_SSP_RA(`UCR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      c1.print();
-      drive_transaction(c1); 
-      
-      repeat(10) begin
-        @(negedge ssp_uart_vif.Clk_sig);
-      end
-
-      //Write data to TDR to go into Transmit FIFO
-      c1 = new(ssp_uart_vif);
-      ssp_di = 'h0F1;
-      c1.set_SSP_RA(`TDR);
-      c1.set_SSP_DI(ssp_di);
-      c1.set_SSP_WnR(`WRITE);
-      c1.set_SSP_SSEL(ssp_ssel);
-      c1.set_SSP_EOC(ssp_eoc);
-      
-      // Call task drive_transaction with config_item c1 as argument
-      for(i=0; i < 9; i++) begin
-        drive_transaction(c1);
-        ssp_di++;
-        c1.set_SSP_DI(ssp_di);
-        repeat(5) begin
-          @(negedge ssp_uart_vif.Clk_sig);
-        end
-      end
+      populateTFIFO(9);
       #150;
  
       c1.set_SSP_WnR(`READ);
@@ -359,4 +409,126 @@ endclass: the_interrupt
 
 
 
-class 
+class tfe_interrupt extends TFIFO_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+    task run();
+      ssp_eoc = 1'b1;
+      ssp_ssel = 1'b1;
+ 
+      clearTFifoInterrupts();
+
+      enableInterrupt();
+
+      populateTFIFO(3);
+     
+      //read out data from TFIFO for interrupt to trigger
+      c1.set_SSP_WnR(`READ);
+      drive_transaction(c1);
+
+      #20us;
+
+      //TODO: Check that iTFE and IRQ go high (also that TxIDLE is asserted)
+      //TODO: De-assert interrupt by reading USR or 
+      //      by writing to tFIFO
+    endtask: run
+
+endclass: tfe_interrupt
+
+
+
+
+//////// Put data in Receive FIFO //////////////
+class rfifo_clear extends RFIFO_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+    task run();
+      ssp_uart_reset();
+      ssp_eoc = 1'b1;
+      ssp_ssel = 1'b1;
+
+      clearFifoInterrupts();
+     
+      populateRFIFO(2);
+      clearRFIFO();
+  
+      #500;
+    endtask: run
+endclass: rfifo_clear
+
+
+
+
+//////// Put data in Receive FIFO //////////////
+class rhf_interrupt extends RFIFO_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+    task run();
+      ssp_uart_reset();
+      ssp_eoc = 1'b1;
+      ssp_ssel = 1'b1;
+      
+      clearFifoInterrupts();
+      enableInterrupt();
+      populateRFIFO(8);
+  
+      #500;
+      //TODO: Check that iRHF and IRQ are asserted
+    endtask: run
+endclass: rhf_interrupt
+
+
+
+
+class interrupt_disable extends TFIFO_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+
+    task run();
+      ssp_eoc = 1'b1;
+      ssp_ssel = 1'b1;
+      ssp_en = 1'b1;
+ 
+      clearTFifoInterrupts();
+
+      populateTFIFO(3);
+     
+      //read out data from TFIFO for interrupt to trigger
+      c1.set_SSP_WnR(`READ);
+      drive_transaction(c1);
+
+      #20us;
+
+      //TODO: Check that iTFE and IRQ never go high (also that TxIDLE is asserted)
+    endtask: run
+
+endclass: interrupt_disable
+
+
+class receiveFifoStatus extends RFIFO_base;
+    function new(virtual ssp_uart_if ssp_uart_vif);
+      super.new(ssp_uart_vif);
+    endfunction: new
+    rand logic [3:0] numTransactions = 'h0;
+    task run();
+      ssp_eoc = 1'b1;
+      ssp_ssel = 1'b1;
+      ssp_en = 1'b1;
+ 
+      clearFifoInterrupts();
+
+      this.randomize();
+      $display("numTransactions being sent = %0d @ time: %0t",numTransactions, $time);
+      populateRFIFO(numTransactions);
+     
+      #20us; 
+    endtask: run
+endclass: receiveFifoStatus
+
